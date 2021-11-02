3cf8bf43356e0f34e827849fb66fd25ca07fe863
naomi-rc <35537740+naomi-rc@users.noreply.github.com>
2021-11-02 01:51:27 -0400

    squeel on game over

--- When Pigs Fly/Assets/Scripts/GameManager.cs
+++ When Pigs Fly/Assets/Scripts/GameManager.cs
@@ -1,28 +1,40 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
-    private Camera mainCamera;
+{    
     public Transform player;
+    public float offset = 10f;
+
     private Vector2 screenBounds;
+    private AudioSource pigCry;    
+    private Camera mainCamera;
+    private bool gameOver = false;
 
     void Start()
     {
         mainCamera = gameObject.GetComponent<Camera>();
         Vector3 position = new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z);
         screenBounds = mainCamera.ScreenToWorldPoint(position);
+        pigCry = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (player.position.y < transform.position.y - screenBounds.y)
+        if (!gameOver && player.position.y < transform.position.y - screenBounds.y)
         {
+            gameOver = true;
             GameOver();
         }
     }
 
     void GameOver()
     {
+        pigCry.Play();
         Debug.Log("GAME OVER!!!");
+    }
+
+    void Restart()
+    {
+
     }
 }
